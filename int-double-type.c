d662d11a6c7661dd9fbb266d48cd13e0f64d1963
Liptan Adhikari <110087917+LiptaNAdhikari@users.noreply.github.com>
2023-08-23 01:03:36 +0600

    int-double-type.c

---
+++ int-double-type.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  
+    // create integer variable
+    int number1 = 34;
+
+    // create double variable
+   double number2 = 78.32;
+
+    // print variables
+    printf("%d %lf", number1, number2);
+
+    return 0;
+ }
